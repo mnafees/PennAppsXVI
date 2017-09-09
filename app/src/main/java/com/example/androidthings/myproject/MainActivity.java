06e951f7d3f03fd0d477a6b750299c701657cfa2
Mohammed Nafees <nafees.technocool@gmail.com>
2017-09-09 10:27:19 -0400

    WiFi and Firebase works

--- app/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -107,7 +107,6 @@ public class MainActivity extends Activity implements
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
-
         myRef.setValue("Hello, World!");
     }
 
