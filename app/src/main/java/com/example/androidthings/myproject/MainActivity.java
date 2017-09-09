e6bf189be3f6aa7098aac5b85c65a16b2aab564d
Mohammed Nafees <nafees.technocool@gmail.com>
2017-09-09 12:14:50 -0400

    Add Nearby API Sub-Pub

--- app/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -16,18 +16,43 @@
 
 package com.example.androidthings.myproject;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.PublishCallback;
+import com.google.android.gms.nearby.messages.PublishOptions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeCallback;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
 import com.google.firebase.FirebaseApp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import im.delight.android.location.SimpleLocation;
+
+import static com.example.androidthings.myproject.R.id;
+import static com.example.androidthings.myproject.R.layout;
 
 /**
  * Skeleton of the main Android Things activity. Implement your device's logic
@@ -48,7 +73,7 @@ import im.delight.android.location.SimpleLocation;
  * is available.
  *
  */
-public class MainActivity extends Activity implements
+public class MainActivity extends FragmentActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
@@ -58,6 +83,21 @@ public class MainActivity extends Activity implements
 
     private SimpleLocation location;
 
+    private static final int TTL_IN_SECONDS = 3 * 60; // Three minutes.
+
+    private static final String KEY_UUID = "key_uuid";
+
+    private static final Strategy PUB_SUB_STRATEGY = new Strategy.Builder()
+            .setTtlSeconds(TTL_IN_SECONDS).build();
+
+    private Switch mPublishSwitch;
+    private Switch mSubscribeSwitch;
+
+    private Message mPubMessage;
+    private MessageListener mMessageListener;
+
+    private ArrayAdapter<String> mNearbyDevicesArrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,13 +105,74 @@ public class MainActivity extends Activity implements
 
         FirebaseApp.initializeApp(this);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Nearby.CONNECTIONS_API)
-                .build();
+        setContentView(layout.activity_main);
 
-        getLocation();
+        mSubscribeSwitch = (Switch) findViewById(id.subscribe_switch);
+        mPublishSwitch = (Switch) findViewById(id.publish_switch);
+
+        // Build the message that is going to be published. This contains the device name and a
+        // UUID.
+        mPubMessage = DeviceMessage.newNearbyMessage(getUUID(getSharedPreferences(
+                getApplicationContext().getPackageName(), Context.MODE_PRIVATE)));
+
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(final Message message) {
+                // Called when a new message is found.
+                mNearbyDevicesArrayAdapter.add(
+                        DeviceMessage.fromNearbyMessage(message).getMessageBody());
+            }
+
+            @Override
+            public void onLost(final Message message) {
+                // Called when a message is no longer detectable nearby.
+                mNearbyDevicesArrayAdapter.remove(
+                        DeviceMessage.fromNearbyMessage(message).getMessageBody());
+            }
+        };
+
+        mSubscribeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // If GoogleApiClient is connected, perform sub actions in response to user action.
+                // If it isn't connected, do nothing, and perform sub actions when it connects (see
+                // onConnected()).
+                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                    if (isChecked) {
+                        subscribe();
+                    } else {
+                        unsubscribe();
+                    }
+                }
+            }
+        });
+
+        mPublishSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // If GoogleApiClient is connected, perform pub actions in response to user action.
+                // If it isn't connected, do nothing, and perform pub actions when it connects (see
+                // onConnected()).
+                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                    if (isChecked) {
+                        publish();
+                    } else {
+                        unpublish();
+                    }
+                }
+            }
+        });
+
+        final List<String> nearbyDevicesArrayList = new ArrayList<>();
+        mNearbyDevicesArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                nearbyDevicesArrayList);
+        final ListView nearbyDevicesListView = (ListView) findViewById(
+                id.nearby_devices_list_view);
+        if (nearbyDevicesListView != null) {
+            nearbyDevicesListView.setAdapter(mNearbyDevicesArrayAdapter);
+        }
+        buildGoogleApiClient();
     }
 
     private void getLocation() {
@@ -92,55 +193,154 @@ public class MainActivity extends Activity implements
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        location.beginUpdates();
-    }
-
-    @Override
-    protected void onPause() {
-        // stop location updates (saves battery)
-        location.endUpdates();
-
-        // ...
-
-        super.onPause();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
+    private static String getUUID(SharedPreferences sharedPreferences) {
+        String uuid = sharedPreferences.getString(KEY_UUID, "");
+        if (TextUtils.isEmpty(uuid)) {
+            uuid = UUID.randomUUID().toString();
+            sharedPreferences.edit().putString(KEY_UUID, uuid).apply();
         }
+        return uuid;
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
+    private void buildGoogleApiClient() {
+        if (mGoogleApiClient != null) {
+            return;
+        }
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        mPublishSwitch.setEnabled(false);
+        mSubscribeSwitch.setEnabled(false);
+        logAndShowSnackbar("Exception while connecting to Google Play services: " +
+                connectionResult.getErrorMessage());
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+        logAndShowSnackbar("Connection suspended. Error code: " + i);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "GoogleApiClient connected");
+        // We use the Switch buttons in the UI to track whether we were previously doing pub/sub (
+        // switch buttons retain state on orientation change). Since the GoogleApiClient disconnects
+        // when the activity is destroyed, foreground pubs/subs do not survive device rotation. Once
+        // this activity is re-created and GoogleApiClient connects, we check the UI and pub/sub
+        // again if necessary.
+        if (mPublishSwitch.isChecked()) {
+            publish();
+        }
+        if (mSubscribeSwitch.isChecked()) {
+            subscribe();
+        }
+    }
+
+    /**
+     * Subscribes to messages from nearby devices and updates the UI if the subscription either
+     * fails or TTLs.
+     */
+    private void subscribe() {
+        Log.i(TAG, "Subscribing");
+        mNearbyDevicesArrayAdapter.clear();
+        SubscribeOptions options = new SubscribeOptions.Builder()
+                .setStrategy(PUB_SUB_STRATEGY)
+                .setCallback(new SubscribeCallback() {
+                    @Override
+                    public void onExpired() {
+                        super.onExpired();
+                        Log.i(TAG, "No longer subscribing");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mSubscribeSwitch.setChecked(false);
+                            }
+                        });
+                    }
+                }).build();
+
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, options)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Subscribed successfully.");
+                        } else {
+                            logAndShowSnackbar("Could not subscribe, status = " + status);
+                            mSubscribeSwitch.setChecked(false);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Publishes a message to nearby devices and updates the UI if the publication either fails or
+     * TTLs.
+     */
+    private void publish() {
+        Log.i(TAG, "Publishing");
+        PublishOptions options = new PublishOptions.Builder()
+                .setStrategy(PUB_SUB_STRATEGY)
+                .setCallback(new PublishCallback() {
+                    @Override
+                    public void onExpired() {
+                        super.onExpired();
+                        Log.i(TAG, "No longer publishing");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mPublishSwitch.setChecked(false);
+                            }
+                        });
+                    }
+                }).build();
+
+        Nearby.Messages.publish(mGoogleApiClient, mPubMessage, options)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Published successfully.");
+                        } else {
+                            logAndShowSnackbar("Could not publish, status = " + status);
+                            mPublishSwitch.setChecked(false);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Stops subscribing to messages from nearby devices.
+     */
+    private void unsubscribe() {
+        Log.i(TAG, "Unsubscribing.");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
+    }
+
+    /**
+     * Stops publishing message to nearby devices.
+     */
+    private void unpublish() {
+        Log.i(TAG, "Unpublishing.");
+        Nearby.Messages.unpublish(mGoogleApiClient, mPubMessage);
+    }
+
+    /**
+     * Logs a message and shows a {@link Snackbar} using {@code text};
+     *
+     * @param text The text used in the Log message and the SnackBar.
+     */
+    private void logAndShowSnackbar(final String text) {
+        Log.w(TAG, text);
+        View container = findViewById(id.activity_main_container);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
 }
